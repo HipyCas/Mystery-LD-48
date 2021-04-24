2a233404111153aa31a86547977768042500354b
Hipy Cas <hipycas@gmail.com>
2021-04-25 00:52:28 +0200

    Add mockup player controller script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+  public List<Hint> collectedHints;
+  public Node[] objectiveNodes;
+  public List<Node> collectedNodes;
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
