6ab1e06e22f60d3fd3f10c574a92435c7f04c8c7
Hipy Cas <hipycas@gmail.com>
2021-04-25 01:16:41 +0200

    Implement requirement checking for Location object

--- Assets/Objects/Location.cs
+++ Assets/Objects/Location.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Location", menuName = "Location/Base")]
@@ -8,9 +9,49 @@ public class Location : ScriptableObject
   public Hint[] hints;
   public Sprite picture;
   public Node[] requiredNodes;
+  public Hint[] requiredHints;
 
   public Hint GetHint()
   {
     return this.hints[(new System.Random()).Next(this.hints.Length)]; // Get a random hint
+  }
+
+  /// <summary> Return true if all required hints and nodes are provided </summary>
+  public bool CanAccess(Hint[] collectedHints, Node[] collectedNodes)
+  {
+    List<bool> allProvided = new List<bool>();
+    // Check for Node requirements, iter over provided for each requirement
+    foreach (Node requiredNode in requiredNodes)
+    {
+      bool isProvided = false; // save if this required node is provided
+      foreach (Node collectedNode in collectedNodes)
+      {
+        if (requiredNode.identifer == collectedNode.identifer)
+        {
+          isProvided = true; // Tell that this one is actually provided
+          break; // And break
+        }
+      }
+      allProvided.Add(isProvided); // append to full requirement list
+    }
+
+    // Same as above
+    foreach (Hint requiredHint in requiredHints)
+    {
+      bool isProvided = false;
+      foreach (Hint collectedHint in collectedHints)
+      {
+        // TODO Get identifier for Hint object instead of checking for name?
+        if (requiredHint.name == collectedHint.name)
+        {
+          isProvided = true;
+          break;
+        }
+      }
+      allProvided.Add(isProvided);
+    }
+
+    if (allProvided.Contains(false)) return false; // Return false if at least one false is in all provided list (if at least one was not provided)
+    else return true; // else if all true, return true
   }
 }
