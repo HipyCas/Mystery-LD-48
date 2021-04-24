bb2341ed16bb5f06b1846a7ed25b940428b815c9
Hipy Cas <hipycas@gmail.com>
2021-04-24 17:40:24 +0200

    Change mouse position collection method, failed

--- Assets/Scripts/LineConnection.cs
+++ Assets/Scripts/LineConnection.cs
@@ -13,18 +13,21 @@ public class LineConnection : MonoBehaviour
   public float zPosition;
   public float xScale = 0.1f;
   public float zScale = 0.1f;
+  public new Camera camera = null; // Overrides default Component.camera
+
+  void Start()
+  {
+    if (this.camera == null) this.camera = Camera.main;
+  }
 
   // Update is called once per frame
   void Update()
   {
     if (this.beingHeldDown)
     {
-      Vector3 mousePos;
-      mousePos = Input.mousePosition;
-      mousePos = new Vector3(mousePos.x, mousePos.y, this.zPosition);
-      mousePos = Camera.main.ScreenToWorldPoint(mousePos, 0); // ! Causing mouse position to reset
+      Transform mousePos = this.GetPointInPlane();
 
-      this.finalPos = new Vector3(mousePos.x, mousePos.y, this.zPosition);
+      this.finalPos = new Vector3(mousePos.position.x, mousePos.position.y, this.zPosition);
 
       float angle = Mathf.Atan2(this.finalPos.y - this.startPos.y, this.finalPos.x - this.startPos.x);
 
@@ -37,11 +40,9 @@ public class LineConnection : MonoBehaviour
   {
     if (Input.GetMouseButtonDown(0))
     {
-      Vector3 mousePos;
-      mousePos = Input.mousePosition;
-      mousePos = Camera.main.ScreenToWorldPoint(mousePos, 0);
+      Transform mouseTransform = this.GetPointInPlane();
 
-      this.startPos = new Vector3(mousePos.x, mousePos.y, this.zPosition);
+      this.startPos = new Vector3(mouseTransform.position.x, mouseTransform.position.y, this.zPosition);
 
       this.connectorObject = Instantiate(this.connectorPrefab, this.startPos, Quaternion.identity);
       this.connectorObject.transform.localScale = new Vector3(this.xScale, 0f, this.zScale);
@@ -53,5 +54,15 @@ public class LineConnection : MonoBehaviour
   private void OnMouseUp()
   {
     this.beingHeldDown = false;
+  }
+
+  private Transform GetPointInPlane()
+  {
+    Ray ray = this.camera.ScreenPointToRay(Input.mousePosition);
+    RaycastHit hit;
+    if (Physics.Raycast(ray, out hit, 100)) // 100 is the max distance the ray reaches (consumes less resources)
+      return hit.transform;
+    else
+      return null;
   }
 }
--- Assets/Scripts/LineConnection2.cs
+++ Assets/Scripts/LineConnection2.cs
@@ -2,18 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LineConnection : MonoBehaviour
+public class LineConnection2 : MonoBehaviour
 {
 
-  public Camera camera;
+  public new Camera camera;
 
   private Transform GetPointInPlane()
   {
     Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-    RayCastHit hit;
-    if(Physics.Raycast(ray, out hit, 100)) // 100 is the max distance the ray reaches (consumes less resources)
+    RaycastHit hit;
+    if (Physics.Raycast(ray, out hit, 100)) // 100 is the max distance the ray reaches (consumes less resources)
     {
       return hit.transform;
     }
+    else return null;
   }
 }
