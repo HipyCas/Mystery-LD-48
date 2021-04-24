cdbff0ab3519c8bce49006fe1ef3992387b2228c
Hipy Cas <hipycas@gmail.com>
2021-04-25 01:17:00 +0200

    Add readonly string identifier
    The identifier consists of the fist hint name plus the second hint name
    joined by an under score (_).

--- Assets/Objects/Node.cs
+++ Assets/Objects/Node.cs
@@ -5,4 +5,10 @@ public class Node : ScriptableObject
 {
   public Hint first;
   public Hint second;
+  public readonly string identifer;
+
+  public Node()
+  {
+    this.identifer = first.name + "_" + second.name;
+  }
 }
