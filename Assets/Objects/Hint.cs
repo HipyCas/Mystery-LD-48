db12e45cc89b6f572977c12eb10b9dc2e6c1ae0f
Hipy Cas <hipycas@gmail.com>
2021-04-24 21:38:11 +0200

    Add hint requirements

--- Assets/Objects/Hint.cs
+++ Assets/Objects/Hint.cs
@@ -7,4 +7,11 @@ public class Hint : ScriptableObject
 {
   public new string name;
   public string description;
+  public Hint[] requiredPreviousHints;
+
+  public bool CanGet()
+  {
+    // TODO Method to check wether you have the requirements to get this hint, to be used in locations
+    return true;
+  }
 }
