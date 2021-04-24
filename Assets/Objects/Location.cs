8aab1728536c980e3100680fc003d7ee56c708b0
Hipy Cas <hipycas@gmail.com>
2021-04-24 21:38:31 +0200

    Create base location scriptable object

---
+++ Assets/Objects/Location.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Location", menuName = "Location/Base")]
+public class Location : ScriptableObject
+{
+  public new string name;
+  public string description;
+  public Hint[] hints;
+  public Sprite picture;
+
+  public Hint GetHint()
+  {
+    return this.hints[(new System.Random()).Next(this.hints.Length)]; // Get a random hint
+  }
+}
